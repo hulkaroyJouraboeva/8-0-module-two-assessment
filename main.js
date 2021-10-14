29a8d0f9d0a08751cde3e80bf33cd9c9817a9453
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-10-14 12:51:22 -0400

    when a movie is selected, movie info is shown

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const dropdown = document.querySelector('select');
+const info = document.querySelector('#display-info');
 
 fetch(`https://ghibliapi.herokuapp.com/films/`)
 .then((response) => response.json())
@@ -8,6 +9,27 @@ fetch(`https://ghibliapi.herokuapp.com/films/`)
         option.setAttribute('value', movie.title);
         option.textContent = movie.title;
         dropdown.append(option);
-        console.log(dropdown);
     });
+
+    dropdown.addEventListener('change', (event) => {
+        movieData.forEach((movie) => {
+
+            if (event.target.value === movie.title) {
+                info.textContent = '';
+                const h3 = document.createElement('h3');
+                info.prepend(h3);
+                h3.textContent = movie.title;
+
+                const p1 = document.createElement('p');
+                info.append(p1);
+                p1.textContent = movie.release_date;
+
+                const p2 = document.createElement('p');
+                info.append(p2);
+                p2.textContent = movie.description;
+            }
+        })
+    })
+
+
 })
